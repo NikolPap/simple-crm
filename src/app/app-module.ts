77183d12be64e3dfd7fde173f4f67a6d4f3cb431
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-24 23:00:35 +0200

    Organisiere AppModule durch Hinzufügen von Dashboard und User Komponenten sowie Bereinigung der Imports

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,23 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon'; 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { Dashboard } from './dashboard/dashboard';
+import { User } from './user/user';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatToolbarModule, 
-    MatSidenavModule,
-    MatIconModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Dashboard, User],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatSidenavModule, MatIconModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
