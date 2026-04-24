2136cf8ad1490ba20a49c5a899a69c781ea66cba
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-24 23:00:42 +0200

    Füge Routing für Dashboard und Benutzer hinzu

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { User } from './user/user';
+import { Dashboard } from './dashboard/dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: Dashboard},
+  {path: 'dashboard', component: Dashboard},
+  {path: 'user', component: User}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
