d955031c6ebfcbf7b843ea22dc99cefa69b398d0
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-25 14:20:11 +0200

    Add DialogAddUser component with template and styles

---
+++ src/app/dialog-add-user/dialog-add-user.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: false,
+  templateUrl: './dialog-add-user.html',
+  styleUrl: './dialog-add-user.scss',
+})
+export class DialogAddUser {}
