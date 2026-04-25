76aaf0f950aee90266c95c796966260121aa4360
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-25 14:18:58 +0200

    Add DialogAddUser component and additional Angular Material modules.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,12 +5,33 @@ import { App } from './app';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { Dashboard } from './dashboard/dashboard';
 import { User } from './user/user';
+import { DialogAddUser } from './dialog-add-user/dialog-add-user';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [App, Dashboard, User],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatSidenavModule, MatIconModule],
+  declarations: [App, Dashboard, User, DialogAddUser],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
