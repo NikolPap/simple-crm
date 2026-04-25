97e9a5fa672f3701461bba31abb32cefc19d8de5
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-25 14:23:03 +0200

    Implement the user component with dialog functionality for user creation.

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
 
 @Component({
   selector: 'app-user',
@@ -6,4 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
-export class User {}
+export class User implements OnInit {
+  constructor(public dialog: MatDialog) {
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
+  openDialog() {
+    this.dialog.open(DialogAddUser);
+  }
+}
