112e6e3ecb7f9b8e40399e69a74451705cc5bebc
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-27 23:24:48 +0200

    feat: display user list in a table

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
+import { Users } from '../../models/user.class';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-user',
@@ -9,12 +11,23 @@ import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
   styleUrl: './user.scss',
 })
 export class User implements OnInit {
-  constructor(public dialog: MatDialog) {
+
+  user= new Users();
+allUsers: any[] = [];
+
+
+  constructor(public dialog: MatDialog, private firestore : AngularFirestore) {
 
   }
   
   ngOnInit(): void {
-    
+    this.firestore
+    .collection('users')
+    .valueChanges()
+    .subscribe((changes: any) => {
+      console.log('Received changes from DB', changes);
+      this.allUsers = changes;
+    })
   }
 
   openDialog() {
