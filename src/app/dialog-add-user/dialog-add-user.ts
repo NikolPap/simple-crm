528c38af851cbc96bcb901d955e029f7dfeabf9e
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-27 23:21:53 +0200

    feat: bind form inputs to user model and setup save method

--- src/app/dialog-add-user/dialog-add-user.ts
+++ src/app/dialog-add-user/dialog-add-user.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Users } from '../../models/user.class';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -6,4 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './dialog-add-user.html',
   styleUrl: './dialog-add-user.scss',
 })
-export class DialogAddUser {}
+export class DialogAddUser {
+  user = new Users();
+  birthDate?: Date;
+
+   constructor(public dialogRef: MatDialogRef<DialogAddUser>, private firestore: AngularFirestore) {
+
+  }
+  ngOnInit(): void {
+
+  }
+
+  saveUser() {
+     if (this.birthDate) {
+      this.user.birthDate = this.birthDate.getTime();
+      console.log('Current user is', this.user);
+      this.firestore
+      .collection('users')
+      .add(this.user.toJSON())
+      .then((result: any) => {
+        console.log('Adding user finished', result);
+        this.dialogRef.close();
+      })
+    }
+}
+}
