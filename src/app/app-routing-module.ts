fb40398a9297db336e348321b7fecd3a04357d90
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-28 23:41:24 +0200

    feat(routing): add user/:id route for user details

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,11 +2,13 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { User } from './user/user';
 import { Dashboard } from './dashboard/dashboard';
+import { UserDetail } from './user-detail/user-detail';
 
 const routes: Routes = [
   {path: '', component: Dashboard},
   {path: 'dashboard', component: Dashboard},
-  {path: 'user', component: User}
+  {path: 'user', component: User},
+  {path: 'user/:id', component: UserDetail}
 ];
 
 @NgModule({
